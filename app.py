084c9fec316bba73d38ddd8c5b0ccab244e93bdf
Muddassir Khalidi <muddassirnawazkhan@gmail.com>
2024-12-06 01:55:46 +0000

    Refining code to be production ready

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 CORS(app)
 
 
 UPLOAD_FOLDER = './recordings'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_audio():
@@ -17,4 +21,5 @@ def upload_audio():
     return jsonify({"message": "Audio uploaded successfully", "file_path": file_path})
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
